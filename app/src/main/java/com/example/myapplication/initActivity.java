abe49b09f8a2c889623de49aeb94caa3d21c89e0
윤영미 <maie421@naver.com>
2022-09-17 23:22:34 +0900

    feat : init 화면 이동

--- app/src/main/java/com/example/myapplication/initActivity.java
+++ app/src/main/java/com/example/myapplication/initActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class initActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class initActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+
+        Button joinBtn = findViewById(R.id.join);
+        joinBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+            startActivity(intent);
+        });
+
+        Button mainBtn = findViewById(R.id.main);
+        mainBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        });
+
+        Button loginBtn = findViewById(R.id.login);
+        loginBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
