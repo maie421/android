581c2ef69c87370833616f37bb6efcdf9698454a
윤영미 <maie421@naver.com>
2022-09-18 00:39:34 +0900

    feat : 하단탭 선택시 화면 이동

--- app/src/main/java/com/example/myapplication/InitActivity.java
+++ app/src/main/java/com/example/myapplication/InitActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class initActivity extends AppCompatActivity {
+public class InitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,7 @@ public class initActivity extends AppCompatActivity {
         Button mainBtn = findViewById(R.id.main);
         mainBtn.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            finishAffinity();
             startActivity(intent);
         });
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = this.getClass().getSimpleName();
+
+    LinearLayout home_ly;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init(); //객체 정의
+        SettingListener(); //리스너 등록
+
+        //맨 처음 시작할 탭 설정
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+
+
+    private void init() {
+        bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
+    }
+
+    private void SettingListener() {
+        //선택 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+//                case R.id.home: {
+//                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                    startActivity(intent);
+//                    return true;
+//                }
+                case R.id.menu_search: {
+                    Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                case R.id.add: {
+                    Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                case R.id.my_page: {
+                    Intent intent = new Intent(getApplicationContext(), MyPageActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
