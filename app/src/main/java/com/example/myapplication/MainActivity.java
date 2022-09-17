8701330c45e6cfed5ee16a2202d4578f870edcef
윤영미 <maie421@naver.com>
2022-09-18 01:57:49 +0900

    .

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,12 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    View searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +27,17 @@ public class MainActivity extends AppCompatActivity {
         //맨 처음 시작할 탭 설정
         bottomNavigationView.setSelectedItemId(R.id.home);
 
+        searchView.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+            finishAffinity();
+            startActivity(intent);
+        });
     }
 
 
     private void init() {
         bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
+        searchView = findViewById(R.id.searchView);
     }
 
     private void SettingListener() {
