6a7add81302cffedf56d1f7951912267509968a4
윤영미 <maie421@naver.com>
2022-09-18 16:01:59 +0900

    feat : 로그인

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        Button loginBtn = findViewById(R.id.login);
+
+        loginBtn.setOnClickListener(view -> {
+            if (email.getText().toString().replace(" ", "").equals("")){
+                Toast.makeText(getApplicationContext(), "아이디 칸이 핀간입니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (password.getText().toString().replace(" ", "").equals("")){
+                Toast.makeText(getApplicationContext(), "비밀번호를 재입력해주세요", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            finishAffinity();
+            intent.putExtra("id",email.getText().toString()); /*송신*/
+            startActivity(intent);
+        });
     }
 }
