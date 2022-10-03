c260746306f6c1f4bdb187a06023ecc1a53b2d53
윤영미 <maie421@naver.com>
2022-10-04 01:08:43 +0900

    feat : 마이페이지

--- app/src/main/java/com/example/myapplication/CouponItem.java
+++ app/src/main/java/com/example/myapplication/CouponItem.java
@@ -22,6 +22,7 @@ public class CouponItem {
     public static ArrayList<CouponItem> giftArrayList = new ArrayList<>();
     public static ArrayList<CouponItem> allArrayList = new ArrayList<>();
     public static ArrayList<CouponItem> purchaseArrayList = new ArrayList<>();
+    public static ArrayList<CouponItem> purchaseAllArrayList = new ArrayList<>();
     public static ArrayList<CouponItem> myItemArrayList = new ArrayList<>();
 
     public CouponItem(String title, String company, String price, String salePrice, String type, String member_idx, Bitmap bitmap) {
@@ -63,12 +64,30 @@ public class CouponItem {
         }
     }
 
+    public static void getPurchaseArrayList(String member_idx) {
+        purchaseArrayList.clear();
+        for (int i =0 ;i< purchaseAllArrayList.size();i++){
+            if (Objects.equals(purchaseAllArrayList.get(i).member_idx, member_idx)){
+                purchaseArrayList.add(new CouponItem(purchaseAllArrayList.get(i).title, purchaseAllArrayList.get(i).company,purchaseAllArrayList.get(i).price,purchaseAllArrayList.get(i).salePrice,purchaseAllArrayList.get(i).type,purchaseAllArrayList.get(i).member_idx,purchaseAllArrayList.get(i).bitmap));
+            }
+        }
+    }
+
+    public static void getMyArrayList(String member_idx) {
+        myItemArrayList.clear();
+        for (int i =0 ;i< allArrayList.size();i++){
+            if (Objects.equals(allArrayList.get(i).member_idx, member_idx)){
+                myItemArrayList.add(new CouponItem(allArrayList.get(i).title, allArrayList.get(i).company,allArrayList.get(i).price,allArrayList.get(i).salePrice,allArrayList.get(i).type,allArrayList.get(i).member_idx,allArrayList.get(i).bitmap));
+            }
+        }
+    }
+
     public static void insertItemArrayList(ArrayList<String> item) {
         allArrayList.add(new CouponItem(item.get(0), item.get(1), item.get(2), item.get(3),item.get(4),item.get(5),StringToBitmap(item.get(6))));
     }
 
     public static void insertPurchaseArrayList(ArrayList<String> item) {
-        purchaseArrayList.add(new CouponItem(item.get(0), item.get(1), item.get(2), item.get(3),item.get(4),item.get(5),StringToBitmap(item.get(6))));
+        purchaseAllArrayList.add(new CouponItem(item.get(0), item.get(1), item.get(2), item.get(3),item.get(4),item.get(5),StringToBitmap(item.get(6))));
     }
 
     public static Bitmap StringToBitmap(String encodedString) {
